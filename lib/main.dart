88bdeb011ee4b430b64f92c27423f50015291788
Aladdin Sonni <aladdin.sonni@gmail.com>
2019-12-02 09:34:00 +0200

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,56 @@
+import 'dart:math';
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int _leftDiceNumber = 1;
+  int _rightDiceNumber = 2;
+
+  void _changeDiceFace() {
+    setState(() {
+      _leftDiceNumber = Random().nextInt(6) + 1;
+      _rightDiceNumber = Random().nextInt(6) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$_leftDiceNumber.png'),
+              onPressed: _changeDiceFace,
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              child: Image.asset('images/dice$_rightDiceNumber.png'),
+              onPressed: _changeDiceFace,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
